c47343ebfc99ae2cd08480464f758d0f9fbb9be8
Justin Glenn Smith <noisesmith@gmail.com>
2017-04-24 10:52:06 -0700

    upgraded deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject littlebird-aviary/conduit "0.0.37"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.371"]
-                 [com.taoensso/encore "1.38.0"]
-                 [com.taoensso/timbre "3.3.1"
+(defproject littlebird-aviary/conduit "0.0.38"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]
+                 [com.taoensso/encore "2.91.0"]
+                 [com.taoensso/timbre "4.10.0"
                   :exclusions [com.taoensso/encore]]
                  [noisesmith/component "0.2.5"]
                  [org.clojure/data.codec "0.1.0"]
-                 [prismatic/schema "0.4.3"]
-                 [clj-kafka "0.3.1"]
-                 [com.taoensso/sente "1.4.1"]
-                 [com.cognitect/transit-clj "0.8.275"]])
+                 [prismatic/schema "1.1.5"]
+                 [clj-kafka "0.3.4"]
+                 [com.taoensso/sente "1.4.1"] ; pinned
+                 [com.cognitect/transit-clj "0.8.300"]])
