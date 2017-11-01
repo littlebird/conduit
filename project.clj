12a90711a41c9251332b48e916d549bd6e89d541
Justin Smith <noisesmith@gmail.com>
2017-10-31 19:27:39 -0700

    deps update

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject littlebird-aviary/conduit "0.2.22"
+(defproject littlebird-aviary/conduit "0.2.46"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]
-                 [com.taoensso/encore "2.91.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 [com.taoensso/encore "2.92.0"]
                  [com.taoensso/timbre "4.10.0"
                   :exclusions [com.taoensso/encore]]
                  [noisesmith/component "0.2.5"]
                  [org.clojure/data.codec "0.1.0"]
-                 [prismatic/schema "1.1.5"]
+                 [prismatic/schema "1.1.7"]
                  [clj-kafka "0.3.4"]
                  [com.taoensso/sente "1.4.1"] ; pinned
                  [com.cognitect/transit-clj "0.8.300"]])
